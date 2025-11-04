88387bdf220ab6dbc71ccdede754cf7316080ec1
Dumildes Paulo <72232653+DumiJDev@users.noreply.github.com>
2025-11-04 20:27:15 +0100

    Refactor Usuario and implement matriculation methods
    Refactor Usuario class to data class and implement matriculation functionality.

--- desafio.kt
+++ desafio.kt
@@ -2,7 +2,7 @@
 
 enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
 
-class Usuario
+data class Usuario(val nome: String, val altura: Double, val idade: Int)
 
 data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
 
@@ -11,11 +11,28 @@ data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) 
     val inscritos = mutableListOf<Usuario>()
     
     fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+        inscritos.add(usuario)
+        println("Aluno ${usuario.nome} foi matriculado")
+    }
+    
+    fun listarMatriculados() {
+        println("Lista dos alunos matriculados\n")
+        for (inscrito in inscritos) {
+            println("- Nome: ${inscrito.nome}")
+        }
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    val conteudos = listOf(ConteudoEducacional(nome = "Introdução a Física"))
+    val usuarios = listOf(Usuario(nome = "John Doe", altura = 1.73, idade = 27))
+    val formacao = Formacao(nome = "Capoeira", conteudos = conteudos)
+    
+    for (usuario in usuarios) {
+        formacao.matricular(usuario)
+    }
+    
+    println()
+    
+    formacao.listarMatriculados()
 }
